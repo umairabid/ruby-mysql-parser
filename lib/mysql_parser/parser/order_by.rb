6b3864f7ac3573d16375b12349e3a2eb11861588
umairabid <umairac90@gmail.com>
2026-05-28 22:06:47 -0700

    Simplify OrderBy to only support plain columns

--- lib/mysql_parser/parser/order_by.rb
+++ lib/mysql_parser/parser/order_by.rb
@@ -1,8 +1,6 @@
 module MysqlParser
   class Parser
     class OrderBy
-      include Helpers
-
       def initialize(lexer)
         @lexer = lexer
       end
@@ -11,7 +9,7 @@ module MysqlParser
         columns = []
 
         loop do
-          column = parse_column
+          column = @lexer.advance
           direction = parse_direction
 
           columns << { column: column, direction: direction }
@@ -25,31 +23,6 @@ module MysqlParser
       end
 
       private
-
-      def parse_column
-        if aggregate_function?
-          name = @lexer.advance
-          "#{name}#{collect_until_close}"
-        else
-          @lexer.advance
-        end
-      end
-
-      def aggregate_function?
-        AGGREGATE_FUNCTIONS.include?(@lexer.current&.downcase) && @lexer.peek == '('
-      end
-
-      def collect_until_close
-        tokens = [@lexer.advance] # (
-        depth = 1
-        while @lexer.current && depth > 0
-          token = @lexer.advance
-          depth += 1 if token == "("
-          depth -= 1 if token == ")"
-          tokens << token
-        end
-        tokens.join("")
-      end
 
       def parse_direction
         token = @lexer.current&.downcase
